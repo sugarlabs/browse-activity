c7e0d7bc00f92c782ea057ee6f83a6511f66a77e
Tomeu <tomeu@bicho.(none)>
2006-12-14 10:24:34 +0100

    Updated to the new clipboard API.

--- webactivity.py
+++ webactivity.py
@@ -21,9 +21,10 @@ import logging
 import dbus
 
 import _sugar
+import sugar.util
 from sugar.activity import ActivityFactory
 from sugar.activity.Activity import Activity
-from sugar.clipboard import ClipboardService
+from sugar.clipboard import clipboardservice
 from sugar import env
 from sugar.graphics import style
 
@@ -122,15 +123,18 @@ def stop():
 
 def download_started_cb(download_manager, download):
     name = download.get_url().rsplit('/', 1)[1]
+    object_id = download.get_file_name() # The file name passed is already unique.
 
-    cbService = ClipboardService.get_instance()
-    cbService.add_object(name,
-                         download.get_mime_type(),
-                         download.get_file_name())
+    cb_service = clipboardservice.get_instance()
+    cb_service.add_object(object_id, name)
+    cb_service.add_object_format(object_id,
+                                 download.get_mime_type(),
+                                 download.get_file_name(),
+                                 on_disk = True)
 
 def download_completed_cb(download_manager, download):
-    cbService = ClipboardService.get_instance()
-    cbService.set_object_state(download.get_file_name(), 100)
+    cb_service = clipboardservice.get_instance()
+    cb_service.set_object_state(download.get_file_name(), 100)
 
 def download_cancelled_cb(download_manager, download):
     #FIXME: Needs to update the state of the object to 'download stopped'.
@@ -139,5 +143,5 @@ def download_cancelled_cb(download_manager, download):
     raise "Cancelling downloads still not implemented."
 
 def download_progress_cb(download_manager, download):
-    cbService = ClipboardService.get_instance()
-    cbService.set_object_state(download.get_file_name(), download.get_percent())
+    cb_service = clipboardservice.get_instance()
+    cb_service.set_object_state(download.get_file_name(), download.get_percent())
