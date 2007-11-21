b735d0b5f946f904077db9f13c8428fc86394524
Simon Schampijer <simon@schampijer.de>
2007-11-22 00:33:02 +0100

    do not resume source with write #4909

--- browser.py
+++ browser.py
@@ -18,9 +18,7 @@
 import logging
 from gettext import gettext as _
 
-import dbus
 import gobject
-import gtk
 import tempfile
 import os
 import time
@@ -33,7 +31,6 @@ from hulahop.webview import WebView
 from sugar.datastore import datastore
 from sugar import profile
 from sugar import env
-from sugar.activity import activityfactory
 from sugar.activity import activity
 
 import sessionstore
@@ -144,15 +141,7 @@ class Browser(WebView):
 
     def _internal_save_cb(self):
         logging.debug("Saved source object to datastore.")
-        object_id = self._jobject.object_id
-        service_name = 'org.laptop.AbiWordActivity'
         self._cleanup_jobject()
-
-        bus = dbus.SessionBus()
-        bus_object = bus.get_object('org.laptop.Journal',
-                                    '/org/laptop/Journal')
-        journal = dbus.Interface(bus_object, 'org.laptop.Journal')
-        journal.Resume(service_name, object_id)
             
     def _internal_save_error_cb(self, err):
         logging.debug("Error saving source object to datastore: %s" % err)
