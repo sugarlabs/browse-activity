8f1932171a2f143e98648a00668d460b907a8c3d
Sascha Silbe <sascha-pgp@silbe.org>
2011-01-31 22:04:19 +0100

    Sort imports
    Signed-off-by: Sascha Silbe <silbe@activitycentral.com>

--- webactivity.py
+++ webactivity.py
@@ -16,9 +16,10 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-import os
 import logging
 from gettext import gettext as _
+import os
+import subprocess
 
 import gobject
 gobject.threads_init()
