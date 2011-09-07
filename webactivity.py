sugarlabs/browse-activity

commit e15a091101223456dc770b9b5e02db8146d05da8

Remove unused imports in webactivity.py
Signed-off-by: Gonzalo Odiard <gonzalo@laptop.org>

webactivity.py
@@ -20,7 +20,6 @@ import logging
 from gettext import gettext as _
 from gettext import ngettext
 import os
-import subprocess
 
 import gobject
 gobject.threads_init()
@@ -177,8 +176,6 @@ from webtoolbar import PrimaryToolbar
 from edittoolbar import EditToolbar
 from viewtoolbar import ViewToolbar
 import downloadmanager
-import globalhistory
-import filepicker
 
 _LIBRARY_PATH = '/usr/share/library-common/index.html'
 

commit 370dfc1c167934eb4784b0444b28a493c4cc6cec

Fix autosuggestions in url entry SL #3087
Fixes regression introduced by e15a091101223456dc770b9b5e02db8146d05da8

Signed-off-by: Simon Schampijer <simon@laptop.org>
Reviewed-by: Gonzalo Odiard <gonzalo@laptop.org>

webactivity.py
@@ -177,6 +177,10 @@ from edittoolbar import EditToolbar
 from viewtoolbar import ViewToolbar
 import downloadmanager
 
+# TODO: make the registration clearer SL #3087
+import globalhistory # pylint: disable=W0611
+import filepicker # pylint: disable=W0611
+
 _LIBRARY_PATH = '/usr/share/library-common/index.html'
 
 from model import Model