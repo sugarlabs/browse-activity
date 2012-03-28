aa109ec239afe692c0ea471670647e0c886e0a92
Manuel Quiñones <manuq@laptop.org>
2012-03-28 15:32:15 -0300

    Pass URI instead of file path when the activity starts from a file
    If the MIME type of the file that the activity reads is not
    "text/plain" or "text/uri-list", it will load the file as-is in the
    default tab.  But WebKit needs an URI, and we are passing the file
    path.  Appending 'file://' to the file path solves the issue.
    
    This fixes second part of #3300 .
    
    Signed-off-by: Manuel Quiñones <manuq@laptop.org>
    Acked-by: Simon Schampijer <simon@laptop.org>

--- webactivity.py
+++ webactivity.py
@@ -400,7 +400,8 @@ class WebActivity(activity.Activity):
                 _logger.error('Open uri-list: Does not support'
                               'list of multiple uris by now.')
         else:
-            self._tabbed_view.props.current_browser.load_uri(file_path)
+            file_uri = 'file://' + file_path
+            self._tabbed_view.props.current_browser.load_uri(file_uri)
 
     def write_file(self, file_path):
         if not self.metadata['mime_type']:
