sugarlabs/browse-activity

commit 04a0de3de3b24ae22bf326de046fb256bdbfeccb

Display 'Loading...' message in the tab and url titles while the page is loading #3550
First of all, we don't need to open "about:blank" when we create a new
tab, this was making the history to save that "page" and allowing to
go back to it.

That is related with this PATCH because we are checking the browser
"load-status" property to know what title put in the tab and url
label. Now, we check for the "load-status" property and set the title
according to the load progress: if WebKit is loading the page we put
"Loading..."  or the page's title otherwise with the exception of
"about:blank" page that has no title, so we put "Untitled".

Signed-off-by: Manuel Kaufmann <humitos@gmail.com>
Acked-by: Manuel Quiñones <manuq@laptop.org>
Acked-by: Simon Schampijer <simon@laptop.org>

webtoolbar.py
@@ -352,6 +352,13 @@ class PrimaryToolbar(ToolbarBase):
         self._update_navigation_buttons()
 
     def __loading_changed_cb(self, widget, param):
+        status = widget.get_load_status()
+        if WebKit.LoadStatus.PROVISIONAL <= status \
+                < WebKit.LoadStatus.FINISHED:
+            self.entry._set_title(_('Loading...'))
+        elif status == WebKit.LoadStatus.FINISHED:
+            if widget.props.title == None:
+                self.entry._set_title(_('Loading...'))
         self._set_status(widget.get_load_status())
 
     def __progress_changed_cb(self, widget, param):

browser.py
@@ -196,7 +196,6 @@ class TabbedView(BrowserNotebook):
         else:
             self._append_tab(browser)
         self.emit('focus-url-entry')
-        browser.load_uri('about:blank')
         return browser
 
     def _insert_tab_next(self, browser):
@@ -360,6 +359,7 @@ class TabLabel(Gtk.HBox):
         GObject.GObject.__init__(self)
 
         browser.connect('notify::title', self.__title_changed_cb)
+        browser.connect('notify::load-status', self.__load_status_changed_cb)
 
         self._label = Gtk.Label(label=_('Untitled'))
         self._label.set_ellipsize(Pango.EllipsizeMode.END)
@@ -397,6 +397,15 @@ class TabLabel(Gtk.HBox):
     def __title_changed_cb(self, widget, param):
         if widget.props.title:
             self._label.set_text(widget.props.title)
+
+    def __load_status_changed_cb(self, widget, param):
+        status = widget.get_load_status()
+        if WebKit.LoadStatus.PROVISIONAL <= status \
+                < WebKit.LoadStatus.FINISHED:
+            self._label.set_text(_('Loading...'))
+        elif status == WebKit.LoadStatus.FINISHED:
+            if widget.props.title == None:
+                self._label.set_text(_('Untitled'))
 
 
 class Browser(WebKit.WebView):

commit 4c7e12ef0f1f1ad452379f609e6cd2f299f950d4

Fix for displaying "Untitled" when the page has no title #3619
This bug was introduced in commit 04a0de3de3b24ae22bf326de046fb256bdbfeccb .

Signed-off-by: Manuel Quiñones <manuq@laptop.org>
Reviewed-By: Manuel Kaufmann <humitos@gmail.com>
Acked-by: Simon Schampijer <simon@laptop.org>

webtoolbar.py
@@ -358,7 +358,7 @@ class PrimaryToolbar(ToolbarBase):
             self.entry._set_title(_('Loading...'))
         elif status == WebKit.LoadStatus.FINISHED:
             if widget.props.title == None:
-                self.entry._set_title(_('Loading...'))
+                self.entry._set_title(_('Untitled'))
         self._set_status(widget.get_load_status())
 
     def __progress_changed_cb(self, widget, param):