sugarlabs/browse-activity

commit 8b0e072b00a306ca98006cc4ba3d3c6a8e7d9c85

Port to Python 3

webactivity.py
@@ -45,7 +45,7 @@ import time
 import shutil
 import json
 import cairo
-import StringIO
+import io
 from hashlib import sha1
 
 from sugar3.activity import activity
@@ -88,7 +88,7 @@ if _profile_version < PROFILE_VERSION:
         with open(os.path.join(_profile_path, 'cert8.db'), 'w') as cert_file:
             cert_file.write()
 
-    os.chmod(os.path.join(_profile_path, 'cert8.db'), 0660)
+    os.chmod(os.path.join(_profile_path, 'cert8.db'), 0o660)
 
     f = open(_version_file, 'w')
     f.write(str(PROFILE_VERSION))
@@ -613,7 +613,7 @@ class WebActivity(activity.Activity):
         Gdk.cairo_set_source_window(cairo_context, window, 0, 0)
         cairo_context.paint()
 
-        thumb_str = StringIO.StringIO()
+        thumb_str = io.StringIO()
         thumb_surface.write_to_png(thumb_str)
         return thumb_str.getvalue()
 

activity/activity.info
@@ -3,7 +3,7 @@ name = Browse
 activity_version = 203.1
 bundle_id = org.laptop.WebActivity
 icon = activity-web
-exec = sugar-activity webactivity.WebActivity -s
+exec = sugar-activity3 webactivity.WebActivity -s
 mime_types = image/png;image/gif;image/jpeg;text/html;text/uri-list;application/xhtml+xml;application/rss+xml;application/xml;text/css;application/ogg;audio/x-wav;audio/ogg;video/ogg;audio/x-vorbis+ogg;audio/x-flac+ogg;audio/x-speex+ogg;video/x-theora+ogg;video/x-ogm+ogg;image/svg+xml;application/x-shockwave-flash
 summary = Surf the world!
 max_participants = 100

browser.py
@@ -478,7 +478,7 @@ class TabbedView(BrowserNotebook):
 
     def get_legacy_history(self):
         tab_histories = []
-        for index in xrange(0, self.get_n_pages()):
+        for index in range(0, self.get_n_pages()):
             tab_page = self.get_nth_page(index)
             tab_histories.append(tab_page.browser.get_legacy_history())
         return tab_histories
@@ -523,14 +523,14 @@ class TabbedView(BrowserNotebook):
 
     def get_state(self):
         state = []
-        for index in xrange(0, self.get_n_pages()):
+        for index in range(0, self.get_n_pages()):
             tab_page = self.get_nth_page(index)
             type_name = TAB_BROWSER
             if isinstance(tab_page, PDFTabPage):
                 type_name = TAB_PDF
             state.append({
                 'type': type_name,
-                'state': tab_page.browser.get_state()})
+                'state': tab_page.browser.get_state().decode()})
         return state
 
     def set_session_state(self, states):
@@ -844,8 +844,6 @@ class Browser(WebKit2.WebView):
         uri = self.get_uri()
         if self.props.title is not None:
             title = self.props.title
-            if not isinstance(title, unicode):
-                title = unicode(title, 'utf-8')
             self._global_history.set_page_title(uri, title)
 
     def __decide_policy_cb(self, webview, policy_decision, decision_type):

downloadmanager.py
@@ -20,7 +20,7 @@ import logging
 from gettext import gettext as _
 import dbus
 import cairo
-import StringIO
+import io
 import tempfile
 
 from gi.repository import Gtk
@@ -290,7 +290,7 @@ class Download(object):
             self.cancel()
             try:
                 datastore.delete(self._object_id)
-            except Exception, e:
+            except Exception as e:
                 logging.warning('Object has been deleted already %s' % e)
 
             self.cleanup()
@@ -406,7 +406,7 @@ class Download(object):
         Gdk.cairo_set_source_pixbuf(cr, pixbuf, 0, 0)
         cr.paint()
 
-        preview_str = StringIO.StringIO()
+        preview_str = io.StringIO()
         preview_surface.write_to_png(preview_str)
         return preview_str.getvalue()
 

linkbutton.py
@@ -21,7 +21,7 @@ from gi.repository import GObject
 from gi.repository import Rsvg
 
 import os
-import StringIO
+import io
 import cairo
 from gettext import gettext as _
 import re
@@ -49,10 +49,6 @@ class LinkButton(TrayButton, GObject.GObject):
     def __init__(self, buf, color, title, owner, hash, notes=None):
         TrayButton.__init__(self)
 
-        # Color read from the Journal may be Unicode, but Rsvg needs
-        # it as single byte string:
-        if isinstance(color, unicode):
-            color = str(color)
         self._fill, self._stroke = color.split(',')
         self.set_image(buf)
 
@@ -78,7 +74,7 @@ class LinkButton(TrayButton, GObject.GObject):
 
     def set_image(self, buf):
         self._img = Gtk.Image()
-        str_buf = StringIO.StringIO(buf)
+        str_buf = io.StringIO(buf)
         thumb_surface = cairo.ImageSurface.create_from_png(str_buf)
 
         xo_buddy = os.path.join(os.path.dirname(__file__), "icons/link.svg")

model.py
@@ -16,7 +16,6 @@
 #    Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 import json
-import sha
 from gi.repository import GObject
 from hashlib import sha1
 
@@ -43,7 +42,7 @@ class Model(GObject.GObject):
 
     def add_link(self, url, title, thumb, owner, color, timestamp,
                  by_me=False):
-        info = {'hash': sha.new(str(url)).hexdigest(), 'url': str(url),
+        info = {'hash': sha1.new(str(url)).hexdigest(), 'url': str(url),
                 'title': str(title), 'thumb': thumb,
                 'owner': str(owner), 'color': str(color),
                 'timestamp': float(timestamp)}

palettes.py
@@ -19,7 +19,7 @@
 import logging
 import os
 import tempfile
-import urllib2
+import urllib.request, urllib.error, urllib.parse
 
 from gettext import gettext as _
 
@@ -243,7 +243,7 @@ class BrowsePalette(Palette):
     def __copy_image_activate_cb(self, menu_item):
         # Download the image
         temp_file = tempfile.NamedTemporaryFile(delete=False)
-        data = urllib2.urlopen(self._image_url).read()
+        data = urllib.request.urlopen(self._image_url).read()
         temp_file.write(data)
         temp_file.close()
 

webtoolbar.py
@@ -92,7 +92,7 @@ css = ('''
     border-left: none;
 }}
 '''.format(thickness=style.LINE_WIDTH))
-css_provider.load_from_data(css)
+css_provider.load_from_data(css.encode('utf-8'))
 context = Gtk.StyleContext()
 context.add_provider_for_screen(screen, css_provider,
                                 Gtk.STYLE_PROVIDER_PRIORITY_USER)
@@ -169,7 +169,7 @@ class WebEntry(iconentry.IconEntry):
     def _search_update(self):
         list_store = Gtk.ListStore(str, str)
 
-        search_text = self.props.text.decode('utf-8')
+        search_text = self.props.text
         for place in places.get_store().search(search_text):
             title = '<span weight="bold" >%s</span>' % (place.title)
             list_store.append([title + '\n' + place.uri, place.uri])
@@ -751,9 +751,6 @@ class PrimaryToolbar(ToolbarBase):
         def create_menu_item(history_item):
             """Create a MenuItem for the back or forward palettes."""
             title = history_item.get_title() or _('No Title')
-            if not isinstance(title, unicode):
-                title = unicode(title, 'utf-8')
-            # This is a fix until the Sugar MenuItem is fixed:
             menu_item = PaletteMenuItem(text_label=title)
             menu_item.connect('activate', self._history_item_activated_cb,
                               history_item)

commit e75d674149e296905e3e9b5358fbd8e1f81e29fe

Encode uri from str to bytes
hash function accepts bytes object.

Regression caused in https://github.com/sugarlabs/browse-activity/commit/8b0e072b00a306ca98006cc4ba3d3c6a8e7d9c85 ("Port to Python 3")

webactivity.py
@@ -411,7 +411,7 @@ class WebActivity(activity.Activity):
     def __link_remove_button_cb(self, button):
         browser = self._tabbed_view.props.current_browser
         uri = browser.get_uri()
-        self.__link_removed_cb(None, sha1(uri).hexdigest())
+        self.__link_removed_cb(None, sha1(uri.encode('utf-8')).hexdigest())
 
     def _go_home_button_cb(self, button):
         self._tabbed_view.load_homepage()